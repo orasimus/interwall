5a7e498a97cb7d231094782eccad8adb9d024ff2
Erik Lanza <erik.lanzaa@gmail.com>
2015-03-20 23:12:02 +0200

    Add a button that plays a sound.

---
+++ Source/Assets/OnClickPlayInterval.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickPlayInterval : MonoBehaviour {
+
+	public AudioClip interval;
+
+	public void PlayInterval()
+	{
+		var audioSource = FindObjectOfType<AudioSource> ();
+		audioSource.PlayOneShot (interval);
+	}
+}
