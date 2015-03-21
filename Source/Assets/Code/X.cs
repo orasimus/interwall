b3636632ba87050ce706f98f836093a1c73171a6
Otso Rasimus <orasimus@gmail.com>
2015-03-21 13:29:32 +0200

    Create dummy level lazily.

--- Source/Assets/Code/X.cs
+++ Source/Assets/Code/X.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 
 public class X : MonoBehaviour
-{	
-	public static Level Level;
+{
+    public static Level Level
+    {
+        get
+        {
+            return new Level()
+            {
+                Interval = "Sounds/ding",
+                CorrectSheet = "A",
+                WrongSheet = "C",
+                WrongSheet2 = "G"
+            };
+        }
+    }
 
 	void Start ()
 	{
-		Level = new Level()
-		{
-			Interval = "Sounds/ding",
-			CorrectSheet = "A",
-			WrongSheet = "C",
-			WrongSheet2 = "G"
-		};
 	}
 }
